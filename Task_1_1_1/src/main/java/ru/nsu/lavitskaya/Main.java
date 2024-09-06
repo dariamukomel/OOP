d49c24916d66f98cf9bc54b7c2744ea8aa10585e
darialavitskaya <117648558+dariamukomel@users.noreply.github.com>
2024-09-06 21:23:41 +0700

    sample

---
+++ Task_1_1_1/src/main/java/ru/nsu/lavitskaya/Main.java
@@ -0,0 +1,10 @@
+package ru.nsu.lavitskaya;
+
+public class Main {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
