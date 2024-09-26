648d658f0a73e3b8ac90a6923c7415af10734a44
Daria Lavitskaya <d.lavitskaya@g.nsu.ru>
2024-09-26 18:57:59 +0700

    commit

---
+++ Task_1_1_3/src/test/java/ru/nsu/lavitskaya/ExpressionParserTest.java
@@ -0,0 +1,12 @@
+﻿package ru.nsu.lavitskaya;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpressionParserTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
